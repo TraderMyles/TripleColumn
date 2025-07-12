ca1aa4322ac7d38d981b194f4d42532b384e947e
TraderMyles <m.will123@hotmail.co.uk>
2025-07-12 15:35:35 +0100

    update mobile app

--- mobile/services/api.js
+++ mobile/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://192.168.1.94:8000',
+  baseURL: 'https://triplecolumn-production.up.railway.app',
 });
 
 export const sendThought = (thought) =>
